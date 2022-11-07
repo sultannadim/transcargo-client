595195131358e662e848d24737389cd7fe149e09
sultan5218 <sultan5218@gmail.com>
2022-11-08 02:17:35 +0600

    footer part complete

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>this is footer</div>;
+  return (
+    <footer className="bg-dark py-4">
+      <div className="container">
+        <div className="row">
+          <div className="text-center">
+            <p className="text-center text-light m-0">
+              &#169; All Rights reserved by Transcargo 2022.
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
