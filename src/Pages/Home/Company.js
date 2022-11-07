f7aeb25a6306081859bcb86b9e519a8cda4bf0f2
sultan5218 <sultan5218@gmail.com>
2022-11-08 03:00:26 +0600

    company part complete

---
+++ src/Pages/Home/Company.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+const Company = () => {
+  return (
+    <section>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="all-company">
+              <img
+                src="https://logistics.stylemixthemes.com/wp-content/uploads/2015/10/tvc-marketing-1.jpg"
+                alt="company"
+                className="img-fluid"
+              />
+              <img
+                src="https://logistics.stylemixthemes.com/wp-content/uploads/2015/10/primo-1.jpg"
+                alt="company"
+                className="img-fluid"
+              />
+              <img
+                src="https://logistics.stylemixthemes.com/wp-content/uploads/2015/10/kphone-1.jpg"
+                alt="company"
+                className="img-fluid"
+              />
+              <img
+                src="https://logistics.stylemixthemes.com/wp-content/uploads/2015/10/edge-1.jpg"
+                alt="company"
+                className="img-fluid"
+              />
+              <img
+                src="https://logistics.stylemixthemes.com/wp-content/uploads/2015/10/arcade-1.jpg"
+                alt="company"
+                className="img-fluid"
+              />
+              <img
+                src="https://logistics.stylemixthemes.com/wp-content/uploads/2015/10/aramiz-1.jpg"
+                alt="company"
+                className="img-fluid"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Company;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Banner from "./Banner";
+import Company from "./Company";
 import "./Home.css";
 import OurWebsite from "./OurWebsite";
 
@@ -8,6 +9,7 @@ const Home = () => {
     <div>
       <Banner></Banner>
       <OurWebsite></OurWebsite>
+      <Company></Company>
     </div>
   );
 };
